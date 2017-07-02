d24563d7ca501453ee88197e65a0de87bb9d219a
Stan4ok <stepandubovoy@yahoo.com>
2017-07-02 06:26:26 +0200

    5.3 HTML Email Template generated

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,23 +7,23 @@ class StaticPagesController < ApplicationController
   	@featured_product = Product.first
   end
   
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @phone = params[:phone] 
-    @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-      to: 'stepandubovoy@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
-  end
-# def thank_you
-#   @name = params[:name]
-#   @email = params[:email]
-#   @phone = params[:phone] 
-#   @message = params[:message] 
-#   UserMailer.contact_form(@email, @name, @phone, @message).deliver_now
-# end
+#  def thank_you
+#    @name = params[:name]
+#    @email = params[:email]
+#    @phone = params[:phone] 
+#    @message = params[:message]
+#    ActionMailer::Base.mail(from: @email,
+#      to: 'stepandubovoy@gmail.com',
+#      subject: "A new contact form message from #{@name}",
+#      body: @message).deliver_now
+#  end
+ def thank_you
+   @name = params[:name]
+   @email = params[:email]
+   @phone = params[:phone] 
+   @message = params[:message] 
+   UserMailer.contact_form(@email, @name, @phone, @message).deliver_now
+ end
 
 
 
