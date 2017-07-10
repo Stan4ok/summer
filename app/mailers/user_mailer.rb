4a599595b9a7190836d3bdc158064489ef1e8892
Stan4ok <stepandubovoy@yahoo.com>
2017-07-10 16:48:58 +0200

    user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,14 +7,14 @@ class UserMailer < ApplicationMailer
     mail(from: email,
           to: 'stepandubovoy@gmail.com',
           subject: "A new contact form message from #{name}"
- # Don't need the body param as we use a template for the body.
+ #      Don't need the body param as we use a template for the body.
  #         body: "#{name} wrote to you: #{@message}",
  #         body: "Contact Telephone: #{phone}"
     )
   end
     def signup_email(user)
   @user = user
-    mail(to: @user.email,
+    mail(to: user.email,
           subject: "Welcome to Bike Berlin!"
     )
   end
