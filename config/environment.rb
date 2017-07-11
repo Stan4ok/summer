986a7e33c35a6eb8e26936e2a17fe53fb1890698
Stan4ok <stepandubovoy@yahoo.com>
2017-07-11 04:37:22 +0200

    SMTP settings configered for sending emails with SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Rails.application.initialize!
 
 #Enable Action Mailer to use the Mailgun plugin to send out emails from Heroku:
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://consumer-app-staging.herokuapp.com/',
-  :authentication => :plain,
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'consumer-app-staging.herokuapp.com',
+  enable_starttls_auto: true
 }
-ActionMailer::Base.delivery_method = :smtp
