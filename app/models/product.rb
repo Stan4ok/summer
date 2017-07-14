7fdd2feed35e1593a426fa68fa8ca126012820e6
Stan4ok <stepandubovoy@yahoo.com>
2017-07-14 08:04:34 +0200

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,13 +7,13 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 	if Rails.env.development?
-		def self.search(search_term)
-  		Product.where("name LIKE ?", "%#{search_term}%")
-		end
+	  def self.search(search_term)
+  	    Product.where("name LIKE ?", "%#{search_term}%")
+	  end
 	else
-		def self.search(search_term)
-			Product.where("name ilike ?", "%#{search_term}%")
-		end
+	  def self.search(search_term)
+	    Product.where("name ilike ?", "%#{search_term}%")
+	  end
 	end
 
   def highest_rating_comment
