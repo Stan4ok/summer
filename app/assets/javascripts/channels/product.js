90c44fb50001a4030dfb062dbd8cc7fa14124fd7
Stan4ok <stepandubovoy@yahoo.com>
2017-07-23 22:11:15 +0200

    6.15 ActionCable connections authenticated, Comments streamed on specific products and through a New job

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -9,7 +9,21 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    $(".comm_alert .alert").show('Drop')
+    $(".comm_alert .alert").show('Drop');
     $(".comm_alert .alert").delay(5000).fadeOut(2050);
+    $('.product_reviews').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
+  },
+
+  listen_to_comments: function(){
+    //As long as you're inside the object of the create() function, you can also simply refer to App.product as this.
+    return this.perform('listen', { 
+      product_id: $("[data-product-id]").data("product-id")
+    });
   }
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
